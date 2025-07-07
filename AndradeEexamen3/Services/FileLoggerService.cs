b07deab546d7cab1927677018e2b04dfe9563bcf
eduardo <eduardo.andrade.cedeno@udla.edu.ec>
2025-07-07 07:33:13 -0500

    Borrador de algo de la carpeta de servicios con el database de los vehiculos y lo del archivo txt

---
+++ AndradeEexamen3/Services/FileLoggerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndradeEexamen3.Services
+{
+    public static class FileLoggerService
+    {
+        public static async Task AppendLogAsync(string texto)
+        {
+            string filename = Path.Combine(FileSystem.AppDataDirectory, "Logs_Andrade.txt");
+            string fecha = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+            await File.AppendAllTextAsync(filename, $"Se incluyó el registro [{texto}] el {fecha}\n");
+        }
+
+        public static async Task<string> LeerLogsAsync()
+        {
+            string filename = Path.Combine(FileSystem.AppDataDirectory, "Logs_Andrade.txt");
+            return File.Exists(filename) ? await File.ReadAllTextAsync(filename) : "Sin registros.";
+        }
+    }
+}
---
+++ AndradeEexamen3/Services/VehiculoDatabase.cs
@@ -0,0 +1,27 @@
+﻿using AndradeEexamen3.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndradeEexamen3.Services
+{
+    class VehiculoDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public VehiculoDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Vehiculo>().Wait();
+        }
+
+        public Task<List<Vehiculo>> GetVehiculosAsync() =>
+            _database.Table<Vehiculo>().ToListAsync();
+
+        public Task<int> SaveVehiculoAsync(Vehiculo vehiculo) =>
+            _database.InsertAsync(vehiculo);
+    }
+}
