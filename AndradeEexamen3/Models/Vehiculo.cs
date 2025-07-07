a1991058c2e665fc3103556af76e67c8aa7b7294
eduardo <eduardo.andrade.cedeno@udla.edu.ec>
2025-07-07 07:23:33 -0500

    Carpeta Modelo con los atributos de Vehiculo agregada

---
+++ AndradeEexamen3/Models/Vehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+using ServiceStack.DataAnnotations;
+
+namespace AndradeEexamen3.Models
+{
+    public class Vehiculo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int AnioFabricacion { get; set; }
+        public string Placa { get; set; }
+    }
+}
