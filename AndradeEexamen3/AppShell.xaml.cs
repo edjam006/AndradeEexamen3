5bb10eed354b72d231722bcbd4ea0a05336314c5
laboratorio <laboratorio@UPOAULA10707>
2025-07-07 07:14:50 -0500

    Agregar archivos de proyecto.

---
+++ AndradeEexamen3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndradeEexamen3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AndradeEexamen3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AndradeEexamen3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
